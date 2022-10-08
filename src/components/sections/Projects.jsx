60a2b3855b7c8dfc55af55a043c4652ef5b7d5f2
Marine Ramillon <marine.ramillon@gmail.com>
2022-10-08 16:52:08 +0200

    final design and cleaning projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
-
-import Button from '../UI/Button';
+import React, { useState } from 'react';
 
 import projectsData from "../../data/projectsData";
-import styled from 'styled-components';
+
+import Button from '../UI/Button';
 import ExternalLink from '../atoms/SVG/ExternalLink';
 import Github from '../atoms/SVG/Github';
+import LeftSvg from '../atoms/SVG/LeftSvg';
+import RightSvg from '../atoms/SVG/RightSvg';
+
+import styled from 'styled-components';
 
 const ProjetsListContainerStyle = styled.div`
-
   display: flex;
   flex-direction: column;
   align-items:center;
@@ -18,123 +20,58 @@ const ProjetsListContainerStyle = styled.div`
   @media(min-width: 768px) {
     flex-direction: row;
   }
-`
+`;
 
 const ProjectListStyle = styled.ul`
-  ${'' /* display: grid;
-  grid-template-columns: 1fr 1fr; */}
-  display: flex;
-  justify-items: center;
-  max-height: 100vh;
-  margin-top: 22px;
-  padding: 0;
-  width: 15%;
+  display: none;
+  padding-left: 0;
 
   li {
     padding-left: 0;
   }
 
-  .project-title-small {
-    font-size: 12px;
-    color: var(--black);
-    margin-bottom: 0;
-    margin-top: 0;
-  }
-
-  .project-icon {
-    display: flex;
-    flex-direction: column;
-
-    p {
-      padding-left: 12px;
-    }
-  }
-
-
   @media(min-width: 768px) {
+    display: flex;
+    width: 150px;
     flex-direction: column;
-    width: 33%;
     padding-right: 0px;
     border-right: 1px solid var(--black);
   }
+`;
 
-`
-
-// const ProjectCardStyle = styled.li`
-//   width: 45px;
-//   height: 45px;
-//   border-radius: 4px;
-//   margin-bottom: 6px;
-//   margin-top: 4px;
-//   ${'' /* margin-left: 12px;
-//   margin-right: 12px; */}
-//   opacity: 1;
-//   position: relative;
-//   cursor: pointer;
-//   border: 1px solid var(--black);
-//   transition: all 200ms ease-in-out;
-
-//   img {
-//     width: 100%;
-//     height: 100%;
-//     top: 0;
-//     left: 0;
-//     position: absolute;
-//     object-fit: cover;
-//     opacity: 0.6;
-//     transition: all 200ms ease-in-out;
-// }
-
-//   .img-active {
-//     opacity: 0.9;
-//   }
-
-//   &.active {
-//     border: 1.5px solid var(--neon-pink);
-//     transform: scale(1.04)
-//     ${'' /* width: 110px;
-//     height: 110px; */}
-//   }
-
-//   @media(min-width: 768px) {
-//     width: 120px;
-//   height: 120px;
-//   margin-left: 12px;
-//   margin-right: 12px;
-
-
-//   }
-// `
-
-const ProjectSelectedStyle = styled.div`
-
-  display: flex;
-  width: 85%;
-
-  ${'' /* padding-left: 80px; */}
-
+const ProjectSelectedStyle = styled.ul`
+  padding-left: 0;
 
   .img-selected {
-    width: 200px;
-    ${'' /* height: 60%; */}
-    position: relative;
-    opacity: 1;
-  ${'' /* border: 1px solid var(--black); */}
+    width: 100%;
   }
 
   img {
     width: 100%;
-    top: 0;
-    left: 0;
-    position: absolute;
     object-fit: cover;
     opacity: 0.8;
-    -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
-  filter: grayscale(100%);
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(100%);
+    border-top: 1px solid var(--black);
+    border-bottom: 1px solid var(--black);
   }
 
   .project-informations {
-    width: 30%;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .project-informations-title {
+    margin-top: 16px;
+    padding-right: 12px;
+    color: var(--neon-pink);
+  }
+
+  .project-links {
+    flex-grow: 1;
+    display: flex;
+    justify-content: end;
   }
 
   .selected-project-title {
@@ -142,14 +79,14 @@ const ProjectSelectedStyle = styled.div`
   }
 
   .tags-container {
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   }
 
   .techno-tag {
     width: fit-content;
     font-size: 12px;
-    color: var(--off-white);
-    background-color: var(--neon-pink);
+    border: 1px solid var(--dark-blue);
     border-radius: 4px;
     margin-right: 8px;
     padding: 0 8px;
@@ -158,63 +95,91 @@ const ProjectSelectedStyle = styled.div`
   @media(min-width: 768px) {
     margin-left: 80px;
 
-  }
+    .project-informations {
+      justify-content: start;
+    }
 
-`
+    .tags-container {
+    display: flex;
+    }
+
+    .img-selected {
+      width: 60%;
+    }
+  }
+`;
 
 
 const Projects = () => {
 
   const [active, setActive] = useState(1);
-  console.log(active)
+
+  const leftProjectHandler = () => {
+    +active === 1 ? setActive(projectsData.length) : setActive(+active -1);
+  };
+
+  const rightProjectHandler = () => {
+    +active === projectsData.length ? setActive(1) : setActive(+active +1);
+  };
 
 
   return (
     <section id="projects">
-        <p className='section-title'>Mes projets</p>
       <ProjetsListContainerStyle>
         <ProjectListStyle>
+          <p className='section-title'>Mes projets</p>
           {projectsData.map(project => (
-            <li><Button className={project.id === +active ? "active" : ""} onClick={(e) => setActive(e.target.id)} id={project.id}>{project.name}</Button></li>
+            <li key={project.id}>
+              <Button
+                className={project.id === +active ? "active" : ""}
+                onClick={(e) => setActive(e.target.id)}
+                id={project.id}>
+                {project.name}
+              </Button>
+            </li>
           ))}
         </ProjectListStyle>
-        {/* <ProjectListStyle>
-          {projectsData.map((project) => (
-            <div className='project-icon'>
-            <p className='project-title-small'>#{project.id} - {project.name}</p>
-            <ProjectCardStyle key={project.id} onClick={(e) => setActive(e.target.id)} className={project.id === +active ? "active" : ""}>
-                <div className="project-list">
-                  <img src={`${process.env.PUBLIC_URL}/images/${project.illustration}`} alt={project.name} id={project.id} className={project.id === +active ? "img-active" : ""}/>
-                </div>
-            </ProjectCardStyle>
-            </div>
-          ))}
-        </ProjectListStyle> */}
-        <ProjectSelectedStyle className='selected-project'>
+
           {projectsData.filter(selected => selected.id === +active).map(project => (
-            <>
+            <ProjectSelectedStyle className='selected-project' key={project.id}>
+              <div className="project-informations" >
+                <LeftSvg
+                  heigth={30}
+                  width={30}
+                  onClick={leftProjectHandler}
+                  className="left-effet"
+                />
+                <h3 className='project-informations-title'>{project.name}</h3>
+                <p>{project.date}</p>
+                <RightSvg
+                  heigth={30}
+                  width={30}
+                  onClick={rightProjectHandler}
+                  className="right-effet"
+                />
+              </div>
               <div className='img-selected'>
                 <img src={`${process.env.PUBLIC_URL}/images/${project.poster}`} alt={project.name} id={project.id}/>
               </div>
+              <div className='project-links'>
+                <a href={project.url}>
+                  <ExternalLink height={24} width={24}/>
+                </a>
+                <a href={project.github}>
+                  <Github height={24} width={24} />
+                </a>
+              </div>
               <div>
-                <h3>{project.name} / {project.date}</h3>
-                <div className='project-informations'>
-                  <a href={project.url}><ExternalLink height={24} width={24}/></a>
-                  <a href={project.github}><Github height={24} width={24} /></a>
-                </div>
                 <div className='tags-container'>
                   {project.tech.map(techno => (<p className='techno-tag'>{techno}</p>))}
                 </div>
                 <p>{project.notice}</p>
               </div>
-            </>
+            </ProjectSelectedStyle>
           ))}
-        </ProjectSelectedStyle>
       </ProjetsListContainerStyle>
     </section>
   )
 };
 
 export default Projects;
-
-        // style={{backgroundImage: `linear-gradient(to bottom, rgba(28,26,28,0.5), rgba(247,31,102,0.2)), url(${`${process.env.PUBLIC_URL}/images/${project.illustration}`})`}}>
